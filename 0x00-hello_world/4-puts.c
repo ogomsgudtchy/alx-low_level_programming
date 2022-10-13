7123c32f96a30435d9a14976a647b232b8b5d357
ogomsgudtchy <ogoegbulem31@yahoo.com>
2022-10-13 11:51:16 -0700

    hello, world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,13 @@
 #include<stdio.h>
 
-/* standard input output */
-
 /**
- *
- * * main - This is where the printing begins
+ * main - This is where the printing begins
  *
  * * Return: Always  0 if successfull.
- *
- * */
+ */
 
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
