c98bf6f1e57a74dc2773a113ff494c7245bf654b
ogomsgudtchy <ogoegbulem31@yahoo.com>
2022-10-13 11:43:55 -0700

    hello, world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/* standard input output */
+
+/**
+ *
+ * * main - This is where the printing begins
+ *
+ * * Return: Always  0 if successfull.
+ *
+ * */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - The main function of printing
+ *
+ * Return: 0 for successful. 1 otherwise
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
