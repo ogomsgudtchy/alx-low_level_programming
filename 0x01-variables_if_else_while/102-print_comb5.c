46ed8e9d43f5ae86ad3fe752fccd4394efc69d8d
ogomsgudtchy <ogoegbulem31@yahoo.com>
2022-10-14 21:42:14 -0700

    variable if else while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,20 +16,20 @@ int main(void)
 	
 	for (p = 0; p <= 98; p++)
 	{
-		for(q = p + 1; q <= 99; q++)
+		for (q = p + 1; q <= 99; q++)
 		{
-			putchar((p / 10) + '0');
-			putchar((p % 10) + '0');
-			putchar(' ');
-			putchar((q / 10) + '0');
-			putchar((q % 10) + '0');
+			putchar ((p / 10) + '0');
+			putchar ((p % 10) + '0');
+			putchar (' ');
+			putchar ((q / 10) + '0');
+			putchar ((q % 10) + '0');
 
 			if (p == 98 && q == 99)
 			continue;
-			putchar(',');
-			putchar(' ');
+			putchar (',');
+			putchar (' ');
 		}
 	}
-	putchar('\n');
+	putchar ('\n');
 	return (0);
 }
