61be634de2c68592962be452fc0a889882aad6cd
ogomsgudtchy <ogoegbulem31@yahoo.com>
2022-10-18 18:27:24 -0700

    functions nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,10 +14,14 @@ void print_to_98(int n)
 			printf("%d, ", n--);
 		printf("%d\n", n);
 	}
-	else
+	else if (n <= 98)
 	{
 		while (n < 98)
 			printf("%d, ", n++);
+		printf("%d, ", n);
+	}
+	else
+	{
 		printf("%d, ", n);
 	}
 }
