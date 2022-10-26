8476e5e671c9d0f2d46b061a60564fa7ea9a12af
ogomsgudtchy <ogoegbulem31@yahoo.com>
2022-10-26 14:17:44 -0700

    more pointers arrays strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - Main Function
+ * @dest: Parameter
+ * @src: parameter
+ * Return: Value
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	for (j = 0; dest[j] != '\0'; j++)
+	{}
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[j + i] = src[i];
+	}
+	dest[j + i] = '\0';
+	return (dest);
+}
