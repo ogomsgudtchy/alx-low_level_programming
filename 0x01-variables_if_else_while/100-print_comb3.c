9fa4a9dd081b60569ed09cdb7ce4edbac910c3cd
ogomsgudtchy <ogoegbulem31@yahoo.com>
2022-10-28 18:20:51 -0700

    variable if else while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
 * main - Entry point
 * Return: Always 0 (Success)
@@ -7,15 +8,16 @@
 int main(void)
 {
 	int i;
+
 	for (i = 0; i < 100; i++)
 	{
-	if (i / 10 < i % 10)
-	if (i < 89)
-	{
-		putchar(44);
-		putchar(32);
+		if (i / 10 < i % 10)
+		if (i < 89)
+		{
+			putchar(44);
+			putchar(32);
+		}
 	}
-	}
-		putchar('\n');
-		return (0);
+	putchar('\n');
+	return (0);
 }
--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,29 +1,34 @@
 #include <stdio.h>
-/**
- * main - program that prints all possible different combinations of three digits
- *  Numbers must be separated by ,followed by a space
- *  The two digits must be different
- *  012, 120, 102, 021, 201, 210 are considered the same combination
- *   Return: 0
 
-*/
+/**
+ * main - prints all possible different combinations of three digits
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	char c;
-	int i;
-	double d;
-	float f;
-
-	printf("Size of a char: %lu", (unsigned long)sizeof(c));
-	printf(" byte(s)\n");
-	printf("Size of an int: %lu", (unsigned long)sizeof(i));
-	printf(" byte(s)\n");
-	printf("Size of a long int: %lu", (unsigned long)sizeof(i));
-	printf(" byte(s)\n");
-	printf("Size of a long long int: %lu", (unsigned long)sizeof(d));
-	printf(" byte(s)\n");
-	printf("Size of a float: %lu", (unsigned long)sizeof(f));
-	printf(" byte(s)\n");
+	int n, m, l;
+	/**/
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 49; m < 58; m++)
+		{
+			for (l = 50; l < 58; l++)
+			{
+				if (l > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
 	return (0);
 }
-
