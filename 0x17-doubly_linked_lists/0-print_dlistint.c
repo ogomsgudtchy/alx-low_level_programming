6080987b94f9fb17b7bb5c204073955c40f646de
ogomsgudtchy <ogoegbulem31@yahoo.com>
2023-01-12 21:06:02 -0800

    Doubly Linked Lists in C

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,16 +6,22 @@
  *
  * Return: The number of nodes in the list.
  */
+
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t nodes = 0;
+	int counter = 0;
 
-	while (h)
+	if (h == NULL)
+		return (counter);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
 	{
-		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
+		counter++;
 	}
-
-	return (nodes);
+	return (counter);
 }
